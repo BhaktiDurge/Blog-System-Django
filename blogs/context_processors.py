1704cbe0eace69218521f07a6368ad09442a8485
BhaktiDurge <durgebhakti15@gmail.com>
2026-07-19 20:18:30 +0530

    blogs, About and search features

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,6 +1,11 @@
 from .models import Category
+from about.models import SocialLinks
 
 def get_categories(request):
     categories = Category.objects.all()
 
     return dict(categories=categories)
+
+def get_SocialLinks(request):
+    social_links = SocialLinks.objects.all()
+    return dict(social_links=social_links)
